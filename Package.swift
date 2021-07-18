f9d1fb9e242bfe7ffe01abc1e6987b7762147bca
Simon Westerlund <simon.westerlund@me.com>
2021-07-19 01:14:20 +0200

    Bump package to iOS 13

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
   name: "combine-schedulers",
   platforms: [
-    .iOS(.v10),
+    .iOS(.v13),
     .macOS(.v10_12),
     .tvOS(.v10),
     .watchOS(.v3),
